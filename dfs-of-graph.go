ff4f03c77f04c03365ba41727308967bb223bca4
Bassel Al Araaj <info@basselalaraaj.com>
2021-03-21 21:59:39 +0100

    fix: use pointers instead

--- dfs-of-graph.go
+++ dfs-of-graph.go
@@ -2,29 +2,27 @@ package main
 
 import "fmt"
 
-func dfs(V int, adj [][]int, visited []int, result []int) []int {
-	visited[V] = 1
-	result = append(result, V)
+func dfs(V int, adj *[][]int, visited *[]int, result *[]int) []int {
+	(*visited)[V] = 1
+	*result = append(*result, V)
 
-	for _, x := range adj[V] {
-		if visited[x] == 0 {
-			res := dfs(x, adj, visited, result)
-			result = res
+	for _, x := range (*adj)[V] {
+		if (*visited)[x] == 0 {
+			dfs(x, adj, visited, result)
 		}
 	}
 
-	return result
+	return *result
 }
 
-func dfsOfGraph(V int, adj [][]int) []int {
+func dfsOfGraph(V int, adj *[][]int) []int {
 	visited := make([]int, V)
 	result := []int{}
 
 	i := 0
 	for i < V {
 		if visited[i] == 0 {
-			res := dfs(i, adj, visited, result)
-			result = append(result, res...)
+			dfs(i, adj, &visited, &result)
 		}
 		i++
 	}
@@ -36,7 +34,7 @@ func main() {
 	V := 5
 	adj := [][]int{{1, 2, 3}, {0}, {0, 4}, {0}, {2}}
 
-	ans := dfsOfGraph(V, adj)
+	ans := dfsOfGraph(V, &adj)
 	fmt.Println(ans)
 
 	//0 1 2 4 3
